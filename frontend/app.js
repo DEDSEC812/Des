31459ffb13713007baa8521759c9d3459b0c338d
8808 <odiuswadsonsayley@gmail.com>
2026-04-25 12:31:34 -0500

    Add files via upload

---
+++ frontend/app.js
@@ -0,0 +1,70 @@
+let allMovies = [];
+
+// LOAD MOVIES
+async function loadMovies() {
+  const res = await fetch("http://localhost:3000/api/videos");
+  const data = await res.json();
+
+  allMovies = data;
+  displayMovies(data);
+}
+
+// DISPLAY FUNCTION
+function displayMovies(movies) {
+  const container = document.getElementById("movies");
+  container.innerHTML = "";
+
+  movies.forEach(v => {
+    container.innerHTML += `
+      <div class="card">
+        <h3>${v.title}</h3>
+        <p>👁️ ${v.views || 0} views</p>
+        <p>❤️ ${v.likes || 0} likes</p>
+
+        <button onclick="watchMovie('${v._id}','${v.videoUrl}')">
+          ▶ Watch
+        </button>
+
+        <button onclick="likeMovie('${v._id}')">
+          ❤️ Like
+        </button>
+      </div>
+    `;
+  });
+}
+
+// SEARCH FUNCTION
+function searchMovie() {
+  const value = document.getElementById("search").value.toLowerCase();
+
+  const filtered = allMovies.filter(v =>
+    v.title.toLowerCase().includes(value)
+  );
+
+  displayMovies(filtered);
+}
+
+// PLAYER
+function playVideo(url) {
+  window.location.href = "player.html?url=" + encodeURIComponent(url);
+}
+
+// INIT
+loadMovies();
+async function watchMovie(id, url) {
+  await fetch(`http://localhost:3000/api/videos/${id}/view`, {
+    method: "PUT"
+  });
+
+  window.location.href =
+    "player.html?id=" + id +
+    "&url=" + encodeURIComponent(url);
+}
+
+async function likeMovie(id) {
+  await fetch(`http://localhost:3000/api/videos/${id}/like`, {
+    method: "PUT"
+  });
+
+  loadMovies();
+}
