b41651c96e2c7dd33a4085b42bcea95c51f762c3
8808 <odiuswadsonsayley@gmail.com>
2026-04-25 12:15:49 -0500

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/videos", require("./routes/videoRoutes"));
+
+app.get("/", (req,res)=>{
+  res.send("CRONCUL API RUNNING");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+  console.log("Server running on port " + PORT);
+});
